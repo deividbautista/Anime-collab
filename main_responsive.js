1a8ea5f78dba51ffb9f03548655c974ab890fbb9
deivid <101598816+deividbautista@users.noreply.github.com>
2023-03-20 16:20:05 -0500

    tercer aporte responsive

---
+++ main_responsive.js
@@ -0,0 +1,11 @@
+const checkbtn = document.querySelector('.checkbtn')
+const check = document.querySelector('.check')
+const menu = document.querySelector('.menu')
+
+
+
+check.addEventListener('click', () =>{
+    menu.classList.toggle('activado');
+})
+
+
