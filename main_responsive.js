b770d98333ff62b2cd1cc6a3e102cca1c64c2f61
deivid <101598816+deividbautista@users.noreply.github.com>
2023-04-03 23:43:30 -0500

    light-mode

--- main_responsive.js
+++ main_responsive.js
@@ -1,12 +1,31 @@
 const check = document.querySelector('.check')
 const menu = document.querySelector('.menu')
-const icon = document.querySelector('.checkbtn')
+const checkbtn = document.querySelector('.checkbtn')
+const lightmode = document.querySelector ('#switch-mode')
+const body = document.querySelector('body')
+load();
 
 
 check.addEventListener('click', () =>{
     menu.classList.toggle('activado');
-    icon.classList.toggle('ñe');
+    checkbtn.classList.toggle('ñe');
 })
 
+lightmode.addEventListener('click', e =>{
+    body.classList.toggle('lightmode');
+    store(body.classList.contains('lightmode'));
+})
+
+function load(){
+    const lightmode = localStorage.getItem('lightmode')
+    if (!lightmode){
+        store('flase');
+    }else if (lightmode== 'true'){
+        body.classList.add('lightmode');
+    }
+}
+function store(value){
+    localStorage.setItem('lightmode', value);
+}
 
 
