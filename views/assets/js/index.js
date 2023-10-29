33ab7dae71daf99444915e94552cfd85290b5bbe
deividbautista <debautistaocampo@gmail.com>
2023-10-29 10:48:09 -0500

    ya sirve

--- views/assets/js/index.js
+++
@@ -1,9 +0,0 @@
-const loginsec=document.querySelector('.login-section')
-const loginlink=document.querySelector('.login-link')
-const registerlink=document.querySelector('.register-link')
-registerlink.addEventListener('click',()=>{
-    loginsec.classList.add('active')
-})
-loginlink.addEventListener('click',()=>{
-    loginsec.classList.remove('active')
-})
